4fbed9a7f4153d9a6065675b7c631a14851de588
mahadihasan2 <mahadihasan125643@gmail.com>
2022-04-08 21:53:32 +0600

    login-register-with-firebase

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAb1VdsRZQrODHNZoyTCtmDmJ_JgUM2KeA",
+  authDomain: "login-authentication-1e3fa.firebaseapp.com",
+  projectId: "login-authentication-1e3fa",
+  storageBucket: "login-authentication-1e3fa.appspot.com",
+  messagingSenderId: "731190236889",
+  appId: "1:731190236889:web:670ae6b0a34175c60dd0c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
